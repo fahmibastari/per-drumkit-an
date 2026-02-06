fa9eaae4604ec115cbd141992eeae6060f99b075
fahmibastari <fahmibastari549@gmail.com>
2026-02-06 14:48:23 +0700

    fix config pwa

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,9 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
-        name: 'CyberDrum Portfolio',
-        short_name: 'CyberDrum',
-        description: 'Professional Online Drum Kit & Portfolio',
+        name: 'Fake Drum',
+        short_name: 'Fake Drum',
+        description: 'Fake Drum',
         theme_color: '#1a1a1a',
         background_color: '#1a1a1a',
         display: 'fullscreen',
